0bc391d888355cd6d9fd374a3f038d27b86e579b
unknown <Ifro@GJM-LAB7-22.ifro.local>
2024-10-02 10:11:53 -0400

    atualização

--- App/Libraries/Database.php
+++ App/Libraries/Database.php
@@ -6,11 +6,12 @@ private $senha = "";
 private $banco = "blogaula";
 private $porta = "3306";
 private $dbh;
+private $stmt;
 
 public function __construct(){
 
 
-        $dns = 'mysql:host='.this->host.';port='.$this->porta.';dbname='.$this->porta.';
+        $dns = 'mysql:host='.$this->host.';port='.$this->porta.';dbname='.$this->porta.';
         dbname='.$this->banco;
 
         $opcoes = [
@@ -28,6 +29,54 @@ public function __construct(){
         die();
         }//fim do catch
 
-        }//fim do método construtor
+     }//fim do método construtor
+// prepara o statement com query 
+public function query($sql){
+        //prepara uma consulta sql
+        $this->stmt = $this->dbh->prepare($sql);
         }
+        
+        //vincula um valor a um parâmetro
+        public function bind($parametro, $valor, $tipo = null){
+        if(is_null($tipo)):
+           switch (true):
+                case is_int($valor):
+                        $tipo = PDO::PARAM_INT;
+                        break;
+                case is_bool($valor):
+                        $tipo = PDO::PARAM_BOOL;
+                        break;
+                case is_null($valor):
+                        $tipo = PDO::PARAM_NULL;
+                        break;
+                default:
+                $tipo = PDO::PARAM_STR;
+                endswitch;
+                endif;
+        
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+        }
+        
+        //executa prepared statement
+        public function executa(){
+                $this ->executa();
+                return $this->stmt->execute();
+        }
+        public function resultado (){
+                $this ->executa();
+                return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+                }
+        //fim da função executa
+        public function resultados (){
+                $this ->executa();
+                return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+          
+        }
+        public function totalResultados(){
+                return $this -> stmt ->rowCount();
+        }//fim da função totalResultados
+}//fim da classe Database
 
+
+
+ 
